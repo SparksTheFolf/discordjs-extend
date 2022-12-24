11efa5e9813592e8efcd2c21e2a0d4bdab8297b2
Nolan <officialstackemup@gmail.com>
2022-12-23 16:39:37 -0800

    slash command update

--- node_modules/discordjs-extend/index.js
+++ node_modules/discordjs-extend/index.js
@@ -1,104 +1,27 @@
 const Discord = require('discord.js');
 const fs = require('node:fs');
 const path = require('path');
-const { Client, GatewayIntentBits, Collection, Events, ActivityType, REST, Routes  } = require('discord.js');
+const { ActivityType } = require('discord.js');
 
 require('dotenv').config();
 
 
 module.exports = {    
-    // Function to create a new Discord client
+
     createClient: (TOKEN, intents) => {
       const client = new Discord.Client({ intents });
       client.login(TOKEN);
       return client;
     },
 
-    showClientInfo: (client) => {
-        console.log('Ready!');
-        console.log(client);
-    },
-
-    registerSlashGuild: (TOKEN, GUILD_ID, CLIENT_ID) => {
-        require('dotenv').config();
-
-        const commands = [];
-        // Grab all the command files from the commands directory you created earlier
-        const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-        
-        // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
-        for (const file of commandFiles) {
-            const command = require(`./commands/${file}`);
-            commands.push(command.data.toJSON());
-        }
-        
-        // Construct and prepare an instance of the REST module
-        const rest = new REST({ version: '10' }).setToken(TOKEN);
-        
-        // and deploy your commands!
-        (async () => {
-            try {
-                console.log(`Started refreshing ${commands.length} application (/) commands.`);
-        
-                // The put method is used to fully refresh all commands in the guild with the current set
-                const data = await rest.put(
-                    Routes.applicationCommands(CLIENT_ID, GUILD_ID),
-                    { body: commands },
-                );
-            
-        
-            // output to the console each file name that was successfully deployed
-                for (const file of commandFiles) {
-                    console.log(`Successfully reloaded ${file}`);
-                }
-        
-                console.log(`Successfully reloaded ${data.length} application (/) commands.`);
-            } catch (error) {
-                // And of course, make sure you catch and log any errors!
-                console.error(error);
-            }
-        })();
-    },
-
-    registerSlashGlobal: (TOKEN, CLIENT_ID) => {
-        require('dotenv').config();
-
-        const commands = [];
-        // Grab all the command files from the commands directory you created earlier
-        const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-        
-        // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
-        for (const file of commandFiles) {
-            const command = require(`./commands/${file}`);
-            commands.push(command.data.toJSON());
-        }
-        
-        // Construct and prepare an instance of the REST module
-        const rest = new REST({ version: '10' }).setToken(TOKEN);
-        
-        // and deploy your commands!
-        (async () => {
-            try {
-                console.log(`Started refreshing ${commands.length} application (/) commands.`);
-        
-                // The put method is used to fully refresh all commands in the guild with the current set
-                const data = await rest.put(
-                    Routes.applicationCommands(CLIENT_ID),
-                    { body: commands },
-                );
-            
-        
-            // output to the console each file name that was successfully deployed
-                for (const file of commandFiles) {
-                    console.log(`Successfully reloaded ${file}`);
-                }
-        
-                console.log(`Successfully reloaded ${data.length} application (/) commands.`);
-            } catch (error) {
-                // And of course, make sure you catch and log any errors!
-                console.error(error);
-            }
-        })();
+    showClientInfo: (aboutclient, guildSize, usersSize) => {
+        console.log('\n');
+        console.log('Loading client...');
+        console.log(aboutclient);
+        console.log(`Serving ${guildSize} servers`);
+		console.log(`Serving ${usersSize} users`);
+        console.log('Client loaded!');
+        console.log('\n');
     },
 
     setPresence: (client, status, activityType, activityName) => {
@@ -108,6 +31,66 @@ module.exports = {
           });
 
         console.log(`Presence set to ${status} ${activityType} ${activityName}`);
-      }
+      },
+
+      //make a function to handle commands from the ./testing/commands folder
+        handleCommands: (client, commandsPath) => {
+
+            const fs = require('node:fs');
+            const {Collection } = require('discord.js');
+
+            client.commands = new Collection();
+
+            const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+            for (const file of commandFiles) {
+                const command = require(`${commandsPath}/${file}`);
+                client.on('interactionCreate', async interaction => {
+                    if (!interaction.isCommand()) return;
+                    if (interaction.commandName === command.data.name) {
+                        await command.execute(interaction);
+                    }
+                });
+                console.log(`Loaded command(s) ${command.data.name}!`);
+            }
+
+        },
+
+        registerSlashGlobal(token, clientId, commandsPath) {
+            const { REST, Routes } = require('discord.js');
+            const fs = require('node:fs');
+
+            const commands = [];
+            const commandFiles = fs.readdirSync(commandsPath + '/').filter(file => file.endsWith('.js'));
+
+            for (const file of commandFiles) {
+                const command = require(commandsPath + '/' + `${file}`);
+                commands.push(command.data.toJSON());
+            }
+
+            // Construct and prepare an instance of the REST module
+            const rest = new REST({ version: '10' }).setToken(token);
+
+            // and deploy your commands!
+            (async () => {
+                try {
+                    console.log(`Started refreshing ${commands.length} application (/) commands.`);
+
+                    // The put method is used to fully refresh all commands in the guild with the current set
+                    const data = await rest.put(
+                        Routes.applicationCommands(clientId),
+                        { body: commands },
+                    );
+
+                    console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+                } catch (error) {
+                    // And of course, make sure you catch and log any errors!
+                    console.error(error);
+                }
+            })();
+        }
+        
+
+    
+
    
 }
